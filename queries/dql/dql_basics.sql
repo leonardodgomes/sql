373c0278b31c41778a38a0506bfaeeb3078058a4
Leonardo Gomes <43643753+leonardodgomes@users.noreply.github.com>
2023-05-24 11:52:20 +0100

    Update dql_basics.sql

--- queries/dql/dql_basics.sql
+++ queries/dql/dql_basics.sql
@@ -1,1 +1,21 @@
+--Data Query Language
+  /*In the context of SQL, "DQL" stands for "Data Query Language." DQL is a subset of SQL (Structured Query Language) that specifically deals 
+    with retrieving or querying data from a database. It includes commands and statements that allow you to retrieve data from one or more 
+    database tables based on specific criteria.
 
+    The primary DQL statement in SQL is the SELECT statement, which is used to retrieve data from a database table or view. The SELECT statement
+    allows you to specify the columns you want to retrieve, the table(s) from which to retrieve the data, and any conditions or filters to apply
+    to the data. It can also include various clauses to sort, group, and aggregate the data.
+
+    Here's a basic example of a SELECT statement in SQL:/*
+
+      SELECT column1, column2, ...
+      FROM table_name
+      WHERE condition;
+
+   /*In this example, column1, column2, and so on represent the columns you want to retrieve from the table_name. The WHERE clause is optional
+     and is used to specify conditions for filtering the data. You can use operators like =, >, <, and others to define the conditions.
+
+     DQL statements provide the means to extract specific information from a database, allowing you to retrieve, filter, and sort data according
+     to your requirements.
+   */
